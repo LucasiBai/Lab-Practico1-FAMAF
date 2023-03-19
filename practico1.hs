63729f859d7dba375b610a34813d65e34fde10bb
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-03-19 15:59:10 -0300

    creacion funciones punto 14 y 15

---
+++ practico1.hs
@@ -0,0 +1,17 @@
+-- 14)
+average :: Float -> Float -> Float
+average first second = (first + second) / 2
+
+-- 15)
+f :: Int -> Int
+f x = 5 * x
+
+duplica :: Int -> Int
+duplica a = a + a
+
+por2 :: Int -> Int
+por2 y = 2*y
+
+multiplicar :: Int -> Int -> Int
+multiplicar zz tt = zz*tt
+
